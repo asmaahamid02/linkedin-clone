4efc2fa4d4549454b0c1b0b754f049c73865e4d3
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-15 15:10:56 +0300

    backend: added login function in auth controller

---
+++ linkedin-backend/controllers/auth.controller.js
@@ -0,0 +1,35 @@
+const User = require('../models/user.model')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const login = async (request, response) => {
+  const { email, password } = request.body
+
+  if (!email || !password)
+    return response
+      .status(404)
+      .json({ message: 'Email and Password are required' })
+  const user = await User.findOne({ email }).select('+password')
+
+  if (!user)
+    return response.status(404).json({ message: 'Invalid Credentials' })
+
+  const isMatch = bcrypt.compare(password, user.password)
+
+  if (!isMatch)
+    return response.status(404).json({ message: 'Invalid Credentials' })
+
+  const token = jwt.sign(
+    { email: user.email, name: user.name },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: '24h',
+    }
+  )
+  response.status(200).json({ message: 'Successfully logged in', data: token })
+}
+
+module.exports = {
+  login,
+  signup,
+}
