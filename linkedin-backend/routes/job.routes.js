60ead7e196afa0747a56979c7e79f2a8ccb0fa57
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-17 00:06:35 +0300

    backend: added getJobs route

--- linkedin-backend/routes/job.routes.js
+++ linkedin-backend/routes/job.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
-const { createJob } = require('../controllers/job.controller')
+const { createJob, getJobs } = require('../controllers/job.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
 const companyMiddleware = require('../middlewares/company.middleware')
 const router = Router()
 
 router.post('/', authMiddleware, companyMiddleware, createJob)
+router.get('/', authMiddleware, companyMiddleware, getJobs)
 
 module.exports = router
