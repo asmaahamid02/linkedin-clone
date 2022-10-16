4c3fcec46de3418219d2914707e818c9f293251e
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-17 00:07:12 +0300

    backend: added followCompany route

--- linkedin-backend/routes/user.routes.js
+++ linkedin-backend/routes/user.routes.js
@@ -1,9 +1,14 @@
 const { Router } = require('express')
-const { updateProfile, applyForJob } = require('../controllers/user.controller')
+const {
+  updateProfile,
+  applyForJob,
+  followCompany,
+} = require('../controllers/user.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
 const userMiddleware = require('../middlewares/user.middleware')
 const router = Router()
 
 router.put('/', authMiddleware, updateProfile)
 router.get('/job/apply/:id', authMiddleware, userMiddleware, applyForJob)
+router.get('/company/follow/:id', authMiddleware, userMiddleware, followCompany)
 module.exports = router
