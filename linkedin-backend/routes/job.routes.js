2ae281c9a2bf7fecad6e9f5dbcb23c0232159c64
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-17 00:30:55 +0300

    backend: added getAppliedUsersByJob route

--- linkedin-backend/routes/job.routes.js
+++ linkedin-backend/routes/job.routes.js
@@ -1,10 +1,15 @@
 const { Router } = require('express')
-const { createJob, getJobs } = require('../controllers/job.controller')
+const {
+  createJob,
+  getJobs,
+  getAppliedUsersByJob,
+} = require('../controllers/job.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
 const companyMiddleware = require('../middlewares/company.middleware')
 const router = Router()
 
 router.post('/', authMiddleware, companyMiddleware, createJob)
 router.get('/', authMiddleware, companyMiddleware, getJobs)
+router.get('/:id', authMiddleware, companyMiddleware, getAppliedUsersByJob)
 
 module.exports = router
