65b0360054f73e6f3193e266e9b6d93ac47d3a51
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-16 22:18:26 +0300

    backend: added applyForJob route

--- linkedin-backend/routes/user.routes.js
+++ linkedin-backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
-const { updateProfile } = require('../controllers/user.controller')
+const { updateProfile, applyForJob } = require('../controllers/user.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
+const userMiddleware = require('../middlewares/user.middleware')
 const router = Router()
 
 router.put('/', authMiddleware, updateProfile)
-
+router.get('/job/apply/:id', authMiddleware, userMiddleware, applyForJob)
 module.exports = router
