46fd49e8ccd7a47a41b705ee1cb7768c5636e107
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-16 22:14:45 +0300

    backend: removed extra declarations

--- linkedin-backend/middlewares/company.middleware.js
+++ linkedin-backend/middlewares/company.middleware.js
@@ -1,10 +1,6 @@
-const jwt = require('jsonwebtoken')
-const Company = require('../models/company.model')
-
 const companyMiddleware = async (request, response, next) => {
   const role = request.user.role
 
-  //if no token
   if (role !== 'company')
     return response.status(401).json({ message: 'Unauthorized' })
 
